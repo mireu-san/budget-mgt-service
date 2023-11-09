c2ad55022f842c8e36df6416fe542c5726f8a105
Chesed Kim <starmireu@gmail.com>
2023-11-09 17:43:04 +0900

    feat : 회원(users) - 사용자 등록 API + 로그인 API 및 JWT 인증

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(
+        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+    )
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
